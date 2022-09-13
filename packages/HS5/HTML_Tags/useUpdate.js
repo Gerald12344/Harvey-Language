7c9cae70e3c2994861686612e8f43f2a58a6a4a1
Harvey Randall <harvey.randall@aol.com>
2022-09-13 16:30:51 +0100

    Cleaned up Server Communication

--- packages/HS5/HTML_Tags/useUpdate.js
+++ packages/HS5/HTML_Tags/useUpdate.js
@@ -1,6 +1,14 @@
 module.exports = {
     Command: function ({ input }) {
-        return `/* USE UPDATE HOOK */if(typeof useUpdateArray === 'undefined'){console.log('%c[Harv-Script]' + '%c - Failed, incorrect use of hooks, useUpdate can only be used inside a hookFunction!','color:#00B5E2','color:white');logError('Failed, incorrect use of hooks, useUpdate can only be used inside a hookFunction!')}else{useUpdateArray.push(useUpdate(${input[0]},${input[1]}))} /* USE UPDATE HOOK END */`;
+        return `
+        /* USE UPDATE HOOK */
+        if(typeof useUpdateArray === 'undefined'){
+            console.log('%c[Harv-Script]' + '%c - Failed, incorrect use of hooks, useUpdate can only be used inside a hookFunction!','color:#00B5E2','color:white');
+            logError('Failed, incorrect use of hooks, useUpdate can only be used inside a hookFunction!')
+        } else {
+            useUpdateArray.push(useUpdate(${input[0]},${input[1]}))
+        } 
+        /* USE UPDATE HOOK END */`.replace(/\n|\r|\t/g, '');
     },
     Dependencies: function () {
         return false;
