54fcf18906477c09eb7f3a2687304c3996f4f0ed
Gerald12344 <harvey.randall@aol.com>
2022-11-01 14:38:20 +0000

    fix hyrdation

--- packages/HS5/HTML_Tags/input.js
+++ packages/HS5/HTML_Tags/input.js
@@ -12,7 +12,7 @@ module.exports = {
             typeIn: 'input',
             splice: 4,
             SSR: {
-                Type: type,
+                type,
             },
         });
     },
